2e14b04bd3b81065a483977386bef54951b9050b
bonnieso <so_bonnie@hotmail.com>
2015-07-12 16:58:18 -0700

    added user to database

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 
-// mongoose.connect(process.env.MONGO_URL);
+ mongoose.connect(process.env.MONGO_URL);
 
  // export MONGO_URL=mongodb://localhost/pokemontrader
 
 var traderSchema = mongoose.Schema({
   trainer_name: { type: String },
+  email: { type: String },
   friend_code: { type: String },
   owned_pokemon: [{name: {type: String}}]
 });
@@ -17,8 +18,21 @@ var Traders = mongoose.model("Traders", traderSchema);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    // console.log(req.user);
-    res.render("index");
+  // console.log(req.user);
+  if(req.user){
+    var trader = new Traders({
+      trainer_name: req.user.displayName,
+      email: req.user.emails[0].value
+    });
+    Traders.findOneAndUpdate({email: req.user.emails[0].value }, trader ,{upsert: true, new: true}, function(err, savedEntry){
+        if (err) {
+        console.log(err);
+        res.status(400).json({ error: "Trainer Saved!" });
+      }
+       console.log('success savedEntry', savedEntry);
+    });
+  }
+  res.render("index");
 });
 
 router.get('/loggedIn', function(req, res, next) {
@@ -41,20 +55,20 @@ router.post("/update", function(req, res){
   });
 });
 
-  router.post("/trader", function(req, res) {
-    console.log(req.user);
-    var trader = new Traders({trainer_name: req.user});
-    // console.log(req.body.trainer_name);
-
-    trader.trainer_name = req.user;
-
-    trader.save(function(err, trainerName) {
-      if (err) {
-        console.log(err);
-        res.status(400).json({ error: "Validation Failed" });
-      }
-      console.log("new trainer:", trainerName);
-      res.json(trainerName);
-    });
-  });
+//  router.post("/trader", function(req, res) {
+//    console.log("trader's", req.user.displayName);
+//    var trader = new Traders({trainer_name: req.user.displayName});
+//    // console.log(req.body.trainer_name);
+//
+//    trader.trainer_name = req.user;
+//
+//    trader.save(function(err, trainerName) {
+//      if (err) {
+//        console.log(err);
+//        res.status(400).json({ error: "Validation Failed" });
+//      }
+//      console.log("new trainer:", trainerName);
+//      res.json(trainerName);
+//    });
+//  });
 module.exports = router;
